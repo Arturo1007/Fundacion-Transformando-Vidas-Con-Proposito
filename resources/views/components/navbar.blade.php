732dd9cdf75fd13396d2f8d6c1f6bb5d9f7ae4ec
Arturo Batista Quiroz <batistaquiroz@gmail.comm>
2022-07-12 15:07:59 -0500

    Arreglo de caligrafia en navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,10 +2,10 @@
     <div class="navbar__logo"> <a href="#"><img src="{{asset('assets/images/logo.png')}}" alt=""></a></div>
     <ul>
         <li class="@if(Route::is('inicio')) active  @endif"><a href="#">INICIO</a></li>
-        <li><a href="#">QUIENES SOMOS</a></li>
+        <li><a href="#">QUIÉNES SOMOS</a></li>
         <li><a href="#">PROYECTOS Y SERVICIOS</a></li>
         <li><a href="#">PROGRAMAS</a></li>
-        <li><a href="#">CONTACTANOS</a></li>
+        <li><a href="#">CONÁCTANOS</a></li>
         <li><a href="#" class="btn-donar">DONAR</a></li>
     </ul>
     <div class="navbar__toggler-container"> <img src="{{'assets/icons/hamburger.svg'}}"  id="nav-toggler"></div>
